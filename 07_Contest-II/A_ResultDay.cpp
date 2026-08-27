a6bf28ed6fc048196b9b50eee3d4e192077cce53
rituraj-stage <rituraj.stage@gmail.com>
2026-08-27 18:35:03 +0530

    contest II ans v1

--- 07_Contest-II/A_ResultDay.cpp
+++ 07_Contest-II/A_ResultDay.cpp
@@ -1,1 +1,28 @@
 // https://codeforces.com/group/4vcXCPx8NY/contest/671030/problem/A
+
+#include <iostream>
+using namespace std;
+void solve(int a[] , int n , int target){
+    int passCount = 0 , failCount = 0;
+    for(int i = 0 ; i < n ; i++){
+        if(a[i] >= target){
+            passCount++;
+        }
+        else{
+            failCount++;
+        }
+    }
+    cout << "Pass: " << passCount << endl;
+    cout << "Fail: " << failCount << endl;
+}
+int main(){
+    int n;
+    cin >> n;
+    int a[n];
+    for(int i = 0 ; i < n ; i++ ){
+        cin >> a[i];
+    }
+    int target;
+    cin >> target;
+    solve(a , n , target);
+}
