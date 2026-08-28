5322ff2c3aff6279d8ccb33fe44bd87a304f3302
rituraj-stage <rituraj.stage@gmail.com>
2026-08-28 19:36:36 +0530

    contest II v1 ans

--- 07_Contest-II/B_FastestRunner.cpp
+++ 07_Contest-II/B_FastestRunner.cpp
@@ -1,1 +1,22 @@
 // https://codeforces.com/group/4vcXCPx8NY/contest/671030/problem/B
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n ;
+    int a[n];
+    for(int i = 0 ; i < n ; i++){
+        cin >> a[i];
+    }
+    int fastest = a[0];
+    int fastestIndex = 0;
+    for(int i = 1 ; i < n ; i++){
+        // cout << a[i] << " ";
+        if(a[i] <= fastest){
+            fastestIndex = i;
+            fastest = a[i];
+        }
+    }
+    cout << fastestIndex+1;
+}
