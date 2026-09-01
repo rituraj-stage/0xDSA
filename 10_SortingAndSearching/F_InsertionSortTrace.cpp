77dd50488889e819aca5d46e8fd9c1d6d7a82921
rituraj-stage <rituraj.stage@gmail.com>
2026-09-01 21:54:39 +0530

    insertion Sort ans

--- 10_SortingAndSearching/F_InsertionSortTrace.cpp
+++ 10_SortingAndSearching/F_InsertionSortTrace.cpp
@@ -28,6 +28,20 @@ void insertionSortTrace(int a[] , int n){
         cout << ", shifts = " << count << endl;
     }
 }
+void insertionSort(int a[] , int n){
+    for(int i = 1 ; i < n ; i++){
+        int temp = a[i];
+        int j = i-1;
+        while(j >= 0 && a[j] > temp){
+            a[j+1] = a[j];
+            j--;
+        }
+        a[j+1] = temp;
+    }
+    for(int i = 0 ; i < n ; i++){
+        cout << a[i] << " ";
+    }
+}
 int main(){
     int n;
     cin >> n;
@@ -36,4 +50,5 @@ int main(){
         cin >> a[i];
     }
     insertionSortTrace(a , n);
+    // insertionSort(a ,n);
 }
