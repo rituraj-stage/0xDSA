336e927e6d295735b9522be7679998df1c4de178
rituraj-stage <rituraj.stage@gmail.com>
2026-09-01 21:52:45 +0530

    insertion Sort ans

--- 10_SortingAndSearching/F_InsertionSortTrace.cpp
+++ 10_SortingAndSearching/F_InsertionSortTrace.cpp
@@ -1,1 +1,39 @@
 // https://codeforces.com/group/4vcXCPx8NY/contest/673406/problem/F
+
+#include<iostream>
+using namespace std;
+void insertionSortTrace(int a[] , int n){
+    for(int i = 1 ; i < n ; i++){
+        int temp = a[i];
+        int j = i-1;
+        int count = 0;
+        while(j >= 0 && a[j] > temp){
+            a[j+1] = a[j];
+            j--;
+            count++;
+        }
+        a[j+1] = temp;
+        cout << "Pass " << i << ": " ;
+        for(int j = 0 ; j < n ; j++){
+            cout << a[j] << " ";
+        }
+        cout << ", ";
+        for(int j = 0 ; j <= i ; j++ ){
+            cout << a[j] << " ";
+        }
+        cout << "| ";
+        for(int j = i+1 ; j < n ; j++){
+            cout << a[j] << " ";
+        }
+        cout << ", shifts = " << count << endl;
+    }
+}
+int main(){
+    int n;
+    cin >> n;
+    int a[n];
+    for(int i = 0 ; i < n ; i++){
+        cin >> a[i];
+    }
+    insertionSortTrace(a , n);
+}
