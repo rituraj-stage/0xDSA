a478b957f1551e829e3ba6a689b2ba60a4566acd
rituraj-stage <rituraj.stage@gmail.com>
2026-09-01 14:04:54 +0530

    bruteForce selectionSort trace

--- 10_SortingAndSearching/D_SelectionSortTrace.cpp
+++ 10_SortingAndSearching/D_SelectionSortTrace.cpp
@@ -1,1 +1,40 @@
 // https://codeforces.com/group/4vcXCPx8NY/contest/673406/problem/D
+
+#include<iostream>
+using namespace std;
+int findMin(int a[] , int i , int n){
+    int min = a[i];
+    int minIndex = i;
+    for(int j = i+1 ; j < n ; j++){
+        if(a[j] < min){
+            min = a[j];
+            minIndex = j;
+        }
+    }
+    return minIndex;
+}
+void selectionSort(int a[] , int n){
+    int  i = 0 ;
+    while(i < n-1){
+        int minIndex = findMin(a , i , n);
+        int min = a[minIndex];
+        int temp = a[i];
+        a[i] = a[minIndex];
+        a[minIndex] = temp;
+        i++;
+        cout << "Pass " << i << ": ";
+        for(int j = 0 ; j < n ; j++){
+            cout << a[j] << " ";
+        }
+        cout << ", min_selected = " << min << endl;
+    }
+}
+int main(){
+    int n;
+    cin >> n;
+    int a[n];
+    for(int i = 0 ; i < n ; i++){
+        cin >> a[i];
+    }
+    selectionSort(a , n);
+}
