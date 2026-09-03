226ca93cad91d525e5596edef1a72d02370574c2
rituraj-stage <rituraj.stage@gmail.com>
2026-09-04 04:10:01 +0530

    T&C ans v1

--- 13_TimeAndSpaceComplexity/A_NaturalSum.cpp
+++ 13_TimeAndSpaceComplexity/A_NaturalSum.cpp
@@ -1,1 +1,9 @@
 // https://codeforces.com/group/4vcXCPx8NY/contest/676977/problem/A
+
+#include<iostream>
+using namespace std;
+int main(){
+    long long n;
+    cin >> n;
+    cout << (n*(n+1))/2;
+}
--- 13_TimeAndSpaceComplexity/B_IntervalSum.cpp
+++ 13_TimeAndSpaceComplexity/B_IntervalSum.cpp
@@ -1,1 +1,15 @@
 // https://codeforces.com/group/4vcXCPx8NY/contest/676977/problem/B
+
+#include<iostream>
+using namespace std;
+int main(){
+    int q;
+    cin >> q;
+    for(int i = 1 ; i <= q ; i++){
+        long long l , r;
+        cin >> l >> r;
+        long long rSum= (r*(r+1))/2;
+        long long lSum= (l*(l-1))/2;
+        cout << rSum - lSum << endl;
+    }
+}
