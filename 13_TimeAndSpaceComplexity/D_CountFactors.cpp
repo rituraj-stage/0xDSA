499e34321f970628e6571fc24fe4cf3207592014
rituraj-stage <rituraj.stage@gmail.com>
2026-09-03 19:36:10 +0530

    ToDo updated

--- 13_TimeAndSpaceComplexity/D_CountFactors.cpp
+++ 13_TimeAndSpaceComplexity/D_CountFactors.cpp
@@ -1,1 +1,26 @@
 // https://codeforces.com/group/4vcXCPx8NY/contest/676977/problem/D
+
+#include <iostream>
+#define int long long
+using namespace std;
+int countFactors(int n){
+    int count = 0;
+    for(int i = 1 ; i*i <= n ; i++){
+        if(n % i == 0){
+            int j = n/i;
+            if(i == j){
+                count++;
+            }
+            else{
+                count+=2;
+            }
+        }
+    }
+    return count;
+}
+signed main(){
+    int n;
+    cin >> n;
+    int count = countFactors(n);
+    cout << count;
+}
--- left.md
+++ left.md
@@ -5,3 +5,13 @@
     `https://codeforces.com/group/4vcXCPx8NY/contest/674948/problem/I`
 - First 4 problems left to solve from Mixed Practice Set.
     `https://codeforces.com/group/4vcXCPx8NY/contest/674948`
+- Contest III
+    `https://codeforces.com/group/4vcXCPx8NY/contest/675648`
+- Time & Space Complexity
+    `https://codeforces.com/group/4vcXCPx8NY/contest/676977`
+
+
+##Contest Option for begineer
+- Atcoder begineer contest
+- Leetcode weekly / biweekly
+- Codeforces Div3 / Div4   
